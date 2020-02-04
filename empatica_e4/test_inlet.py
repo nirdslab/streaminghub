986e87d035a7b966967f27cc764d8450b7fead6d
yasith <yasith@cs.odu.edu>
2020-02-04 04:08:30 -0500

    make test inlet and outlet async

--- empatica_e4/test_inlet.py
+++ empatica_e4/test_inlet.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
-from threading import Thread
-from time import sleep
+import asyncio
 
 from pylsl import StreamInlet, resolve_stream
 
 
-def listener(inlet):
+async def listen(inlet: StreamInlet):
     while True:
-        sample, timestamp = inlet.pull_sample()
-        print('\t'.join(map(lambda x: str(x).ljust(20), [inlet.info().type(), timestamp, sample])), flush=True)
-        sleep(1)
+        samples, timestamps = inlet.pull_chunk()
+        for sample, timestamp in zip(samples, timestamps):
+            print('\t'.join(map(lambda x: str(x).ljust(20), [inlet.info().type(), timestamp, sample])), flush=True)
+        await asyncio.sleep(0.1)
 
 
-if __name__ == '__main__':
+async def main():
     print("looking for an Empatica E4 stream...")
     streams = resolve_stream('name', 'Empatica E4')
     assert len(streams) > 0
     print(f'{len(streams)} Stream(s) found!')
 
     # create a new inlet to read from the stream
-    threads = list(map(lambda x: Thread(target=listener, args=(StreamInlet(x),)), streams))
+    jobs = [listen(StreamInlet(stream)) for stream in streams]
     print('starting listeners')
-    for thread in threads:
-        thread.start()
-    threads[0].join()
+    await asyncio.gather(*jobs)
+
+
+if __name__ == '__main__':
+    try:
+        print('starting listeners...')
+        asyncio.get_event_loop().run_until_complete(main())
+    except KeyboardInterrupt:
+        print('stopped listening')
