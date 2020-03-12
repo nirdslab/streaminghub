92b142c57de7e67708559feffb5ee65b14a36210
yasith <yasith@cs.odu.edu>
2020-03-12 00:29:09 -0400

    get important fields from gaze and fixation topics

--- pupil_labs/connector.py
+++ pupil_labs/connector.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python
+import asyncio
 import io
-from pprint import pprint
 from typing import Any
 
 import msgpack
 import zmq
+import zmq.asyncio
 from PIL import Image
 
 ZMQ_REQ = zmq.REQ
 ZMQ_SUB = zmq.SUB
 ZMQ_SUBSCRIBE = zmq.SUBSCRIBE
+CONF_THRESHOLD = 0.8
 
 
 class Parser(object):
 
     def __init__(self, topic) -> None:
         super().__init__()
-        self.topic = topic
+        self.topic: str = topic
 
     def parse(self, message, extra) -> Any:
-        print(f"{self.topic} - {message}")
+        t = self.topic
+        if self.topic.startswith('gaze'):
+            [gaze_pos_x, gaze_pos_y] = message[b'norm_pos']
+            gaze_ts = message[b'timestamp']
+            gaze_conf = message[b'confidence']
+            if gaze_conf > CONF_THRESHOLD:
+                print(f"[{t}](t={gaze_ts:.2f}, c={gaze_conf:.2f}): x: {gaze_pos_x:.3f}, y: {gaze_pos_y:.3f}")
+        if self.topic.startswith('fixations'):
+            [fxn_pos_x, fxn_pos_y] = message[b'norm_pos']
+            fxn_ts = message[b'timestamp']
+            fxn_conf = message[b'confidence']
+            fxn_d = message[b'duration']
+            fxn_dispersion = message[b'dispersion']
+            if fxn_conf > CONF_THRESHOLD:
+                print(f"[{t}](t={fxn_ts:.2f}, c={fxn_conf:.2f}): x: {fxn_pos_x:.3f}, y: {fxn_pos_y:.3f}, r: {fxn_dispersion:.3f}, d: {fxn_d:.3f}")
 
         # TODO do something if topic = 'frame' and len(extra) > 0. use topic to find what the image is (eye 1 / eye 2 / world)
         # print("\n{}:{}".format('topic', msg.get('topic')))
@@ -52,12 +68,12 @@ class Parser(object):
         #     pass
 
 
-def main():
+async def main():
     # remote host / port
     _host: str = '127.0.0.1'
     _port: int = 50020
     _sub_port: int = ...
-    _ctx = zmq.Context()
+    _ctx = zmq.asyncio.Context()
 
     # GET SUB_PORT
     print('# GET SUB_PORT #')
@@ -69,7 +85,7 @@ def main():
     # receive SUB port
     print('Querying SUB_PORT...', end=' ', flush=True)
     _req.send_string('SUB_PORT')
-    _sub_port = int(_req.recv_string())
+    _sub_port = int(await _req.recv_string())
     print(f'{_sub_port}')
     # close REQ socket
     print('Closing REQ socket...', end=' ', flush=True)
@@ -85,9 +101,9 @@ def main():
 
     # subscribe to streams
     _sub.setsockopt_string(ZMQ_SUBSCRIBE, 'gaze')  # where you're looking at
-    _sub.setsockopt_string(ZMQ_SUBSCRIBE, 'pupil')  # information about pupil (such as diameter, dilation)
+    # _sub.setsockopt_string(ZMQ_SUBSCRIBE, 'pupil')  # information about pupil (such as diameter, dilation)
     _sub.setsockopt_string(ZMQ_SUBSCRIBE, 'fixations')  # starts sending data when it detects fixations (continuously sends until next fixation)
-    _sub.setsockopt_string(ZMQ_SUBSCRIBE, 'frame')  # camera stream (eye, world)
+    # _sub.setsockopt_string(ZMQ_SUBSCRIBE, 'frame')  # camera stream (eye, world)
     # _sub.setsockopt_string(ZMQ_SUBSCRIBE, 'notify') # notifications
     # _sub.setsockopt_string(ZMQ_SUBSCRIBE, 'logging') # for logging
 
@@ -96,7 +112,7 @@ def main():
     while True:
         try:
             # receive raw data (bytes)
-            data = _sub.recv_multipart()
+            data = await _sub.recv_multipart()
             # content of data after being parsed
             [topic, payload, *_extra] = data
             topic = topic.decode()
@@ -112,11 +128,9 @@ def main():
                     print('Unsupported byte data received, ignoring.')
             # parse data
             Parser(topic).parse(message, _extra)
-        except KeyboardInterrupt:
+        except InterruptedError:
             break
-        except BaseException as e:
-            pprint(e)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
