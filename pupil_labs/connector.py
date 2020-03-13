edcd6d90051652b727797badb5fbe63e6293171e
yasith <yasith@cs.odu.edu>
2020-03-13 06:55:46 -0400

    remove unneeded comments

--- pupil_labs/connector.py
+++ pupil_labs/connector.py
@@ -36,36 +36,7 @@ class Parser(object):
             fxn_dispersion = message[b'dispersion']
             if fxn_conf > CONF_THRESHOLD:
                 print(f"[{t}](t={fxn_ts:.2f}, c={fxn_conf:.2f}): x: {fxn_pos_x:.3f}, y: {fxn_pos_y:.3f}, r: {fxn_dispersion:.3f}, d: {fxn_d:.3f}")
-
         # TODO do something if topic = 'frame' and len(extra) > 0. use topic to find what the image is (eye 1 / eye 2 / world)
-        # print("\n{}:{}".format('topic', msg.get('topic')))
-        # # print("{}:{}".format('base_data', msg.get('base_data')))
-        # print("{}:{}".format('timestamp', msg.get('timestamp')))
-        # print("{}:{}".format('norm_pos', msg.get('norm_pos')))
-        # print("{}:{}".format('confidence', msg.get('confidence')))
-
-        # Fixations
-        # print("\n{}:{}".format('topic',msg.get('topic')))
-        # print("{}:{}".format('method',msg.get('method')))
-        # print("{}:{}".format('norm_pos',msg.get('norm_pos')))
-        # print("{}:{}".format('duration',msg.get('duration')))
-        # print("{}:{}".format('confidence',msg.get('confidence')))
-        # print("{}:{}".format('timestamp',msg.get('timestamp')))
-        # print("{}:{}".format('dispersion',msg.get('dispersion')))
-
-        # surfaces = loads(msg, encoding='utf-8')
-        # filtered_surface = {k: v for k, v in surfaces.items() if surfaces['name'] == surface_name}
-        # try:
-        #     # note that we may have more than one gaze position data point (this is expected behavior)
-        #     gaze_positions = filtered_surface['gaze_on_surfaces']
-        #     for gaze_pos in gaze_positions:
-        #         norm_gp_x, norm_gp_y = gaze_pos['norm_pos']
-        #
-        #         # only print normalized gaze positions within the surface bounds
-        #         if (0 <= norm_gp_x <= 1 and 0 <= norm_gp_y <= 1):
-        #             print("gaze on surface: {}\t, normalized coordinates: {},{}".format(surface_name, norm_gp_x, norm_gp_y))
-        # except:
-        #     pass
 
 
 async def main():
