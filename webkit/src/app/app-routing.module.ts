5f4daf1fcddfda72cb795cca92ddeceaa2cb7b38
yasith <yasith.jayawardana@icloud.com>
2020-10-10 01:00:09 -0400

    create initial component set

--- webkit/src/app/app-routing.module.ts
+++ webkit/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DesignerComponent } from './views/designer/designer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DesignerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- webkit/src/app/app.module.ts
+++ webkit/src/app/app.module.ts
@@ -1,16 +1,35 @@
+// Angular Modules
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { MatInputModule } from '@angular/material/input'
+// Application Components
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './components/canvas/canvas.component';
+import { EditorComponent } from './components/editor/editor.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { WidgetComponent } from './components/widget/widget.component';
+import { DesignerComponent } from './views/designer/designer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent,
+    EditorComponent,
+    MenuComponent,
+    HeaderComponent,
+    FooterComponent,
+    WidgetComponent,
+    DesignerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
