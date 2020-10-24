ea7518147c45c95f5fbe2e8cfb64b4bb38d250e1
yasith <yasith.jayawardana@icloud.com>
2020-10-24 04:27:52 -0400

    simplify config

--- webflow/native/index.ts
+++ webflow/native/index.ts
@@ -2,8 +2,8 @@ import { app, BrowserWindow } from 'electron';
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     webPreferences: {
       nodeIntegration: true
     }
