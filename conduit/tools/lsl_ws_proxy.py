0f0f0106701da94fbf8a3afeb8fe09785b114e7e
yasith <yasith.jayawardana@icloud.com>
2020-11-23 15:54:33 -0500

    optimize stream generation and termination logic

--- conduit/tools/lsl_ws_proxy.py
+++ conduit/tools/lsl_ws_proxy.py
@@ -3,7 +3,7 @@
 import asyncio
 import json
 import threading
-from typing import List
+from typing import List, Callable
 
 import numpy as np
 import websockets
@@ -13,15 +13,19 @@ from tools.util import stream_info_to_dict
 
 
 async def ws_handler(websocket: websockets.WebSocketServerProtocol, path: str):
+    async def callback(data):
+        await ws_push(data, websocket)
+
     async for message in websocket:
         print(f"< {message}")
-        await consumer(message, websocket)
+        await consumer(message, callback)
 
 
-async def consumer(message: str, websocket: websockets.WebSocketServerProtocol):
+async def consumer(message: str, callback: Callable):
     payload = json.loads(message)
     command = payload['command']
     response = {'command': command, 'error': None, 'data': None}
+
     if command == 'search':
         streams = resolve_stream()
         response['data'] = {'streams': [*map(stream_info_to_dict, streams)]}
@@ -30,10 +34,10 @@ async def consumer(message: str, websocket: websockets.WebSocketServerProtocol):
         source_id, source_name, source_type = data['id'], data['name'], data['type']
         # create stream inlets to pull data from
         streams: List[StreamInlet] = [StreamInlet(x, max_chunklen=1, recover=False) for x in resolve_stream('source_id', source_id) if x.name() == source_name and x.type() == source_type]
-        # create async jobs to push data through the websocket
-        thread = threading.Thread(target=create_streaming_task, args=(streams, websocket))
+        # create thread (with its own event loop) to proxy LSL data into websockets
+        thread = threading.Thread(target=create_lsl_proxy, args=(streams, callback))
         thread.start()
-    await ws_push(response, websocket)
+    await callback(response)
 
 
 async def ws_push(payload: dict, websocket: websockets.WebSocketServerProtocol):
@@ -42,29 +46,27 @@ async def ws_push(payload: dict, websocket: websockets.WebSocketServerProtocol):
     await websocket.send(message)
 
 
-def create_streaming_task(streams: List[StreamInlet], websocket: websockets.WebSocketServerProtocol):
+def create_lsl_proxy(streams: List[StreamInlet], push: Callable):
     # create an event loop and begin data stream
     inner_loop = asyncio.new_event_loop()
     asyncio.set_event_loop(inner_loop)
-    inner_loop.run_until_complete(proxy_stream(streams, websocket))
+    inner_loop.run_until_complete(lsl_connector(streams, push))
     inner_loop.close()
 
 
-async def proxy_stream(streams: List[StreamInlet], websocket: websockets.WebSocketServerProtocol):
-    thread = threading.current_thread()
-    thread.alive = True
-    while thread.alive:
-        for stream in streams:
-            try:
+async def lsl_connector(streams: List[StreamInlet], push: Callable):
+    keepalive = True
+    while keepalive:
+        try:
+            for stream in streams:
                 sample, timestamps = stream.pull_chunk(0.0)
                 typ = stream.info().type()
                 if len(sample) == 0:
                     continue
-                await ws_push({'command': 'data', 'data': {'stream': typ, 'chunk': np.nan_to_num(sample, nan=-1).tolist()}}, websocket)
-            except LostError:
-                thread.alive = False
-                break
-    if not thread.alive:
+                await push({'command': 'data', 'data': {'stream': typ, 'chunk': np.nan_to_num(sample, nan=-1).tolist()}})
+        except LostError:
+            keepalive = False
+    else:
         print('stream unsubscribed')
 
 
