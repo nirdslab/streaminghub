189ba018883bb628827265fe79f042f0f7ec19d1
yasith <yasith.jayawardana@icloud.com>
2021-07-01 22:20:00 -0400

    add more form fields

--- webui/src/app/editor/editor.component.ts
+++ webui/src/app/editor/editor.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import loader from '@monaco-editor/loader';
-import templates from './templates';
+import { templates, Datasource, Dataset, Analytic } from './templates';
 
 @Component({
   selector: 'app-editor',
@@ -11,6 +11,12 @@ export class EditorComponent implements OnInit, AfterViewInit {
 
   @ViewChild("editor", { static: false }) editorElement?: ElementRef;
 
+  public selection: 'DATASET' | 'DATASOURCE' | 'ANALYTIC' = 'DATASET';
+
+  public datasetSpec: Dataset = { ...templates.dataset };
+  public datasourceSpec: Datasource = { ...templates.datasource };
+  public analyticSpec: Analytic = { ...templates.analytic };
+
   constructor() {
   }
 
@@ -19,8 +25,16 @@ export class EditorComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     loader.init().then(monaco => {
-      monaco.languages.json.jsonDefaults.setDiagnosticsOptions({ validate: true, allowComments: false, schemas: [], enableSchemaRequest: true});
-      monaco.editor.create(this.editorElement?.nativeElement, { value: JSON.stringify(templates.dataset, undefined, 2), language: 'json' });
+      monaco.languages.json.jsonDefaults.setDiagnosticsOptions({ validate: true, allowComments: false, schemas: [], enableSchemaRequest: true });
+      let json = null;
+      if (this.selection == "DATASET") {
+        json = this.datasetSpec;
+      } else if (this.selection == "DATASOURCE") {
+        json = this.datasourceSpec;
+      } else if (this.selection == "ANALYTIC") {
+        json = this.analyticSpec;
+      }
+      monaco.editor.create(this.editorElement?.nativeElement, { value: JSON.stringify(json, undefined, 2), language: 'json' });
     });
   }
 
--- webui/src/app/editor/templates.ts
+++ webui/src/app/editor/templates.ts
@@ -1,4 +1,70 @@
-const templates = {
+interface Info {
+  timestamp: string;
+  version: string;
+  checksum: string;
+}
+
+interface Field {
+  name: string;
+  description: string;
+  dtype: string;
+}
+
+interface Stream {
+  name: string;
+  index: string;
+  description: string;
+  unit: string;
+  frequency: string;
+  channels: string[];
+}
+
+interface Author {
+  name: string;
+  affiliation: string;
+  email: string;
+}
+
+interface Group {
+  description: string;
+  attributes: string[];
+}
+
+export interface Datasource {
+  $schema: string;
+  info: Info;
+  device: {
+    model: string;
+    manufacturer: string;
+    category: string;
+  };
+  fields: { [id: string]: Field };
+  streams: { [id: string]: Stream };
+}
+
+export interface Dataset {
+  $schema: string;
+  name: string;
+  description: string;
+  keywords: string[];
+  authors: Author[];
+  groups: { [id: string]: Group };
+  info: Info;
+  sources: { [id: string]: string };
+  fields: { [id: string]: Field };
+  resolver: string;
+}
+
+export interface Analytic {
+  $schema: string;
+  info: Info;
+  sources: { [id: string]: string };
+  fields: { [id: string]: Field };
+  inputs: { [id: string]: string };
+  streams: { [id: string]: Stream };
+}
+
+export const templates = {
   "datasource": {
     "$schema": "https://raw.githubusercontent.com/nirdslab/streaminghub/master/dfs/datasource.schema.json",
     "info": {
