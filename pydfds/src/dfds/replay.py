3ad1edac5639455df1271291046c7fb0fd32a29b
Yasith Jayawardana <yasith@cs.odu.edu>
2023-04-12 04:30:05 -0400

    fix bugs

--- pydfds/src/dfds/replay.py
+++ pydfds/src/dfds/replay.py
@@ -2,8 +2,7 @@ import logging
 from typing import Dict
 
 import pylsl
-
-from .typing import Source
+from dtypes import Source, Stream
 
 logger = logging.getLogger()
 
@@ -12,7 +11,8 @@ def replay_stream(
     source_id: str,
     source: Source,
     stream_id: str,
-    attrs: Dict[str, str] = None,
+    stream: Stream,
+    attrs: Dict[str, str],
 ) -> pylsl.StreamOutlet:
     """
     Generate LSL outlet from Metadata
@@ -28,17 +28,18 @@ def replay_stream(
         source_id=source_id,
         type=stream_id,  # TODO do not use for queries
         name=f"{source_id}-{stream_id}",  # TODO do not use for queries
-        channel_count=len(stream.channels),
+        channel_count=len(stream.fields) + len(stream.index),
         nominal_srate=stream.frequency,
     )
     # create stream description
     desc = info.desc()
 
     # add device information
-    device_info = desc.append_child("device")
-    device_info.append_child_value("model", source.model)
-    device_info.append_child_value("manufacturer", source.manufacturer)
-    device_info.append_child_value("category", source.category)
+    if source.device is not None:
+        device_info = desc.append_child("device")
+        device_info.append_child_value("model", source.device.model)
+        device_info.append_child_value("manufacturer", source.device.manufacturer)
+        device_info.append_child_value("category", source.device.category)
 
     # add attributes if present
     attributes = desc.append_child("attributes")
@@ -50,31 +51,27 @@ def replay_stream(
 
     # add stream information
     stream_info = desc.append_child("stream")
-    # add channel information to stream information
-    channels_info = stream_info.append_child("channels")
-    for channel_id in stream.channels.keys():
-        channel_info = channels_info.append_child(channel_id)
-        # add channel properties
-        channel_props = stream.channels[channel_id]
-        for channel_prop_id in channel_props.keys():
-            channel_info.append_child_value(
-                channel_prop_id, channel_props[channel_prop_id]
-            )
-    # add description and frequency
-    stream_info.append_child_value("description", stream.description)
-    stream_info.append_child_value("frequency", str(stream.frequency))
-    # add indexes information
-    stream_idxs_info = stream_info.append_child("index")
-    for index_id in stream.index.keys():
-        stream_idx_info = stream_idxs_info.append_child(index_id)
-        stream_idx_props = stream.index[index_id]
-        for stream_idx_prop_id in stream_idx_props.keys():
-            stream_idx_info.append_child_value(
-                stream_idx_prop_id, stream_idx_props[stream_idx_prop_id]
-            )
-    # add name and unit
     stream_info.append_child_value("name", stream.name)
+    stream_info.append_child_value("description", stream.description)
     stream_info.append_child_value("unit", stream.unit)
+    stream_info.append_child_value("frequency", str(stream.frequency))
+
+    # add field information to stream information
+    field_info = stream_info.append_child("channels")
+    for field_id in stream.fields.keys():
+        field_info = field_info.append_child(field_id)
+        # add data field properties
+        field = stream.fields[field_id]
+        field_info.append_child_value("name", field.name)
+        field_info.append_child_value("description", field.description)
+
+    index_info = stream_info.append_child("index")
+    for field_id in stream.index.keys():
+        field_info = index_info.append_child(field_id)
+        # add index field properties
+        field = stream.index[field_id]
+        field_info.append_child_value("name", field.name)
+        field_info.append_child_value("description", field.description)
 
     # return stream outlet
     return pylsl.StreamOutlet(info)
