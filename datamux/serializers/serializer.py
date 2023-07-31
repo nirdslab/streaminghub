c125b8f80725ee7a6329388067166b215ee6024c
Yasith Jayawardana <yasith@cs.odu.edu>
2023-07-31 10:58:09 -0400

    temp commit

--- datamux/serializers/serializer.py
+++ datamux/serializers/serializer.py
@@ -7,20 +7,24 @@ import avro.datafile
 import avro.io
 import avro.schema
 
-avro_def = {
-    "type": "record",
-    "namespace": "Data",
-    "name": "datapofloat_schema",
-    "fields": [
-        {"name": "t", "type": "float", "doc": "Column t"},
-        {"name": "x", "type": "float", "doc": "Column x"},
-        {"name": "y", "type": "float", "doc": "column y"},
-        {"name": "dL", "type": "float", "doc": "Column dL"},
-        {"name": "dR", "type": "float", "doc": "Column dR"},
-        {"name": "aoi", "type": "float", "doc": "Column aoi"},
-    ],
-}
 
+avro_schemas: dict[str, avro.schema.RecordSchema] = {}
+
+
+def get_avro_schema(
+    topic: bytes,
+    content: dict | None = None,
+) -> avro.schema.RecordSchema:
+    name = f"datamux.{topic.decode()}"
+    if name not in avro_schemas:
+        assert content is not None
+        fields = [dict(name=k, type=type(v).__name__) for k,v in content.items()]
+        avro_schemas["name"] = avro.schema.RecordSchema(
+            name=name,
+            namespace="streaminghub",
+            fields=fields
+        )
+    return avro_schemas["name"]
 
 class Serializer:
     def __init__(
@@ -47,7 +51,7 @@ class Serializer:
         if len(content) == 0:
             content_enc = b""
         else:
-            content_enc = self.encode_fn(content)
+            content_enc = self.encode_fn(topic, content)
         payload = topic + b"|" + content_enc
         return payload
 
@@ -58,30 +62,35 @@ class Serializer:
         topic, content_enc = payload.split(b"|", maxsplit=1)
         content = {}
         if len(content_enc) > 0:
-            content = self.decode_fn(content_enc)
+            content = self.decode_fn(topic, content_enc)
         return topic, content
 
     def __encode_avro(
         self,
+        topic: bytes,
         content: dict,
     ) -> bytes:
-        data = [content]
-        schema = avro.schema.make_avsc_object(avro_def)
+        schema = get_avro_schema(topic, content)
         buffer = io.BytesIO()
-        avro.datafil
         encoder = avro.io.BinaryEncoder(buffer)
-        self.writer.write(schema, content, encoder)
+        self.writer.write_record(schema, content, encoder) # type: ignore
         content_bytes = buffer.getvalue()
         return content_bytes
 
     def __decode_avro(
         self,
+        topic: bytes,
         content_bytes: bytes,
     ) -> dict:
-        raise NotImplementedError()
+        schema = get_avro_schema(topic)
+        buffer = io.BytesIO(content_bytes)
+        decoder = avro.io.BinaryDecoder(buffer)
+        content = self.reader.read_record(schema, schema, decoder)
+        return dict(content)
 
     def __encode_json(
         self,
+        topic: bytes,
         content: dict,
     ) -> bytes:
         content_str = json.dumps(content)
@@ -90,6 +99,7 @@ class Serializer:
 
     def __decode_json(
         self,
+        topic: bytes,
         content_bytes: bytes,
     ) -> dict:
         content_str = content_bytes.decode()
