734da94cb79ad9d2309201c9ecfa90ecc4341256
Yasith Jayawardana <mail@yasith.dev>
2024-07-15 17:03:27 -0400

    add write_config to dfds

--- streaminghub_pydfds/src/streaminghub_pydfds/__init__.py
+++ streaminghub_pydfds/src/streaminghub_pydfds/__init__.py
@@ -1,3 +1,3 @@
 from .typing import *
 from .parser import Parser
-from .util import load_config
+from .util import load_config, write_config
--- streaminghub_pydfds/src/streaminghub_pydfds/util.py
+++ streaminghub_pydfds/src/streaminghub_pydfds/util.py
@@ -3,11 +3,25 @@ from pathlib import Path
 
 from .typing import Config
 
+config_paths = [
+    Path("~/.streaminghubrc").expanduser().resolve(),
+    Path(".streaminghubrc").resolve(),
+]
+
 
 def load_config() -> Config:
-    config_path = Path("~/.streaminghubrc").expanduser().resolve()
-    assert config_path.exists() and config_path.is_file()
-    with open(config_path) as f:
-        data = json.load(f)
+    data = {}
+    for path in config_paths:
+        if path.exists() and path.is_file():
+            with open(path) as f:
+                patch = json.load(f)
+                data.update(patch)
     config = Config(**data)
     return config
+
+
+def write_config(config: Config):
+    # update the highest priority file
+    path = config_paths[-1]
+    with open(path, "w") as f:
+        json.dump(config.model_dump(), f)
